6c8135a915d1e51405d11bac3a2ef28302ad8097
luisgiedroyc <costyakozloff@gmail.com>
2026-02-13 15:16:33 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cook.app',
+  appName: 'cook',
+  webDir: 'dist'
+};
+
+export default config;
+server: {
+  androidScheme: "https"
+},
+webDir: "dist"
